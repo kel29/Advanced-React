393706e821e476f3082ee57fa0d44805158f98cb
Krista Loven <krista.loven.87@gmail.com>
2020-06-17 09:23:20 -0700

    initial pages

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
+
+const Nav = (props) => {
+  return (
+    <NavStyles>
+      <Link href="/">
+        <a>
+          Items
+        </a>
+      </Link>
+      <Link href="/sell">
+        <a>
+          Sell
+        </a>
+      </Link>
+      <Link href="/signup">
+        <a>
+          Signup
+        </a>
+      </Link>
+      <Link href="/orders">
+        <a>
+          Orders
+        </a>
+      </Link>
+      <Link href="/me">
+        <a>
+          Account
+        </a>
+      </Link>
+    </NavStyles>
+  );
+};
+
+export default Nav;
