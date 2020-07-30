f5e4e56c9d2cfdaf49697f9b2a64c564f6f14d66
Krista Loven <krista.loven.87@gmail.com>
2020-07-30 10:39:22 -0700

    Update items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -2,11 +2,17 @@ const Mutations = {
   async createItem(parent, args, context, info) {
     // TODO, check if logged in
     const item = await context.db.mutation.createItem({
-      data: {
-        ...args
-      }
+      data: { ...args }
     }, info)
     return item
+  },
+  async updateItem(parent, args, context, info) {
+    const { id, ...updates } = args
+    return context.db.mutation.updateItem({
+      data: updates,
+      where: { id },
+      info
+    })
   }
 };
 
