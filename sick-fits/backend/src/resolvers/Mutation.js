75a9d33220a66197b83e78dadbf9a87ac024d601
Krista Loven <krista.loven.87@gmail.com>
2020-08-07 08:48:28 -0700

    Signup - BE

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const Mutations = {
   async createItem(parent, args, context, info) {
     // TODO, check if logged in
@@ -18,7 +21,26 @@ const Mutations = {
     const where = { id: args.id}
     const item = await context.db.query.item({where}, `{id, title}`)
     return context.db.mutation.deleteItem({where}, info)
-  }
+  },
+  async signup(parent, args, context, info) {
+    const { email: rawEmail, password: rawPassword } = args
+    const email = rawEmail.toLowerCase()
+    const password = await bcrypt.hash(rawPassword, 10)
+    const user = await context.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        email,
+        permissions: { set: ['USER'] }
+      }
+    }, info)
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    context.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 20 * 60 * 24 * 365 // one year cookie
+    })
+    return user
+  } 
 };
 
 module.exports = Mutations;
