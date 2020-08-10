c996b35e63f146f5537227daca91b0f18c16c188
Krista Loven <krista.loven.87@gmail.com>
2020-08-10 10:54:39 -0700

    Permissions (BE)

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding')
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -12,6 +13,13 @@ const Query = {
     return context.db.query.user({
       where: { id: userId}
     }, info)
+  },
+  async users(parent, args, context, info) {
+    if (!context.request.userId) {
+      throw new Error('You must be logged in to continue')
+    }
+    hasPermission(context.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
+    return context.db.query.users({}, info)
   },
 };
 
